cc7e40b7d8083a8056b39330d44a734d1c7b5696
Connor Turland <connorturland@gmail.com>
2019-01-31 16:05:07 +0100

    tweak main.js

--- public/main.js
+++ public/main.js
@@ -1,13 +1,13 @@
 rtb.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'State of Affairs Tree',
+        title: 'Update SoA Tree Data',
         svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
         onClick: () => {
           rtb.board.getAllObjects().then(results => {
             //let allWidgets = results.filter(x => x.type != "FRAME")
-
+            rtb.showNotification('Data is being saved to https://soa-tree.herokuapp.com')
             fetch('https://a65ecbed.ngrok.io/update-soa-tree-data', {
               method: 'POST',
               headers: {
@@ -15,7 +15,7 @@ rtb.initialize({
               },
               body: JSON.stringify(results)
             }).then(() => {
-              rtb.showNotification('Data was saved. Check out https://f5f8dd0b.ngrok.io for visualizations of the tree.')
+              rtb.showNotification('Data was saved. Check out https://soa-tree.herokuapp.com for visualizations of the tree.')
             })
           })          
         }
