d2993004e82b38ffe4e78f161bff9cad61c54cb0
Will Harris-Braun <willharrisbraun@gmail.com>
2019-08-06 10:51:02 -0400

    Fix set-complete icon

--- public/set-complete.js
+++ public/set-complete.js
@@ -1,7 +1,6 @@
 rtb.onReady(() => {
   const completeIcon = `
-  <circle cx="12" cy="12" r="8" fill="#8fd14f" fill-rule="evenodd" stroke="#0ca789" stroke-width="2.5"/>
-  <circle cx="12" cy="12" r="10" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2.5"/>`
+  <circle cx="12" cy="12" r="10" fill="#8fd14f" fill-rule="evenodd" stroke="currentColor" stroke-width="2.5"/>`
 
   rtb.initialize({
     extensionPoints: {
