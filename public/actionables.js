dd2afeec01da0fa12cab4e427e9e70add2f5be9a
Will Harris-Braun <willharrisbraun@gmail.com>
2019-08-06 13:23:57 -0400

    Add Actionables icon

--- public/actionables.js
+++ public/actionables.js
@@ -1,7 +1,7 @@
 rtb.onReady(() => {
   const actionablesIcon = `
   <circle cx="12" cy="12" r="10" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2.5"/>
-  <circle fill="#8E8E8E" cx="16.6" cy="14.2" r="0.8"/>
+  <text transform="matrix(1 5.886198e-04 -5.886198e-04 1 5.7983 17.3127)" fill="#619319" font-family="'Futura'" font-size="16.3442px">A</text>
 `
   rtb.initialize({
     extensionPoints: {
